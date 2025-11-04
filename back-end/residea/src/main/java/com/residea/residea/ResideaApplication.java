a01be9f7bf53dc0fb97583bd16f91629aa901e41
MarcoDima02 <dimamarco02@gmail.com>
2025-11-04 17:36:18 +0100

    inizializzazione spring boot

---
+++ back-end/residea/src/main/java/com/residea/residea/ResideaApplication.java
@@ -0,0 +1,13 @@
+package com.residea.residea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResideaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResideaApplication.class, args);
+	}
+
+}
---
+++ back-end/residea/src/test/java/com/residea/residea/ResideaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.residea.residea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResideaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
