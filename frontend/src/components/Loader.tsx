485d9bc23ed08bd61f039f1aebc51bc56d018c12
Max <massimo.musso@edu-its.it>
2025-11-05 21:50:51 +0100

    Creazione layout e inizializzazione delle rotte

---
+++ frontend/src/components/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="flex items-center justify-center h-screen text-primary">
+      <div className="animate-spin rounded-full h-8 w-8 border-4 border-primary border-t-transparent" />
+    </div>
+  );
+};
+
+export default Loader;
