bedb23e8c03ce08fa84ffc117ca1aeb3476206c8
Max <massimo.musso@edu-its.it>
2025-11-05 19:48:16 +0100

    Inizializzate librerie per lo stile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,31 @@
-import { Button } from "./components/ui/button";
+import { Button } from './components/ui/button';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 
 const App = () => {
   return (
     <div className="p-8">
       <Button>Ciao</Button>
+
+      <Dialog>
+        <DialogTrigger>Open</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
