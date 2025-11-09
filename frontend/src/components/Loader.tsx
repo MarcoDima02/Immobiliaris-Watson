db8f7bd33325619b8cd707014be9d74470ad4cb0
Max-HKW <massimo.musso@edu-its.it>
2025-11-09 10:37:00 +0100

    Delete frontend directory

--- frontend/src/components/Loader.tsx
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-  return (
-    <div className="flex items-center justify-center h-screen text-primary">
-      <div className="animate-spin rounded-full h-8 w-8 border-4 border-primary border-t-transparent" />
-    </div>
-  );
-};
-
-export default Loader;
