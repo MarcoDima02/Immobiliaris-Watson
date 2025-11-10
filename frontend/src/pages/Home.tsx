beb4cdc78aaad66eea79aff8274c43335a54acd0
davide-its <davide.martinelli@edu-its.it>
2025-11-10 12:36:04 +0100

    Minor fix

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -41,7 +41,7 @@ const Home = () => {
       </section>
 
       <div className="absolute top-[25%] z-10 left-1/2 lg:left-50 transform -translate-x-1/2 lg:translate-x-0 px-4 sm:px-6 w-full max-w-sm sm:max-w-md md:max-w-lg p-8 rounded-xl text-center lg:text-left">
-        <h1 className="text-5xl font-bold text-zinc-800 mb-3 max-w-[20ch] md:max-w-[35ch]">
+        <h1 className="text-5xl font-bold text-zinc mb-3 max-w-[20ch] md:max-w-[35ch]">
           Vuoi vendere casa senza stress?
         </h1>
         <h2 className="text-lg font-semibold mb-4 max-w-[30ch] md:max-w-[45ch]">
@@ -57,8 +57,8 @@ const Home = () => {
         <div className="flex justify-center flex-col items-center gap-10">
 
           <div className='text-center'>
-            <h2 className='text-4xl font-bold text-zinc-800'>Come funziona</h2>
-            <h3 className='text-lg font-semibold text-zinc-800'>Ricevi in tre step la valutazione del tuo immobile!</h3>
+            <h2 className='text-4xl font-bold text-zinc'>Come funziona</h2>
+            <h3 className='text-lg font-semibold text-zinc'>Ricevi in tre step la valutazione del tuo immobile!</h3>
           </div>
 
           <div className='row flex flex-row flex-wrap justify-center items-start gap-10 lg:gap-0'>
@@ -67,7 +67,7 @@ const Home = () => {
               <div>
                 <div className='bg-card rounded-full p-3 shadow-xl w-20 h-20 flex justify-center items-center'><File className='h-10 w-10 text-foreground' /></div>
               </div>
-              <div className=' text-left text-lg font-medium text-zinc-800'>
+              <div className=' text-left text-lg font-medium text-zinc'>
                 <p><b>Compila il form</b> con i dati dell’abitazione in pochi minuti</p>
               </div>
             </div>
@@ -76,7 +76,7 @@ const Home = () => {
               <div>
                 <div className='bg-card rounded-full p-3 shadow-xl w-20 h-20 flex justify-center items-center'><Euro className='h-10 w-10 text-foreground' /></div>
               </div>
-              <div className=' text-left text-lg font-medium text-zinc-800'>
+              <div className=' text-left text-lg font-medium text-zinc'>
                 <p><b>Ricevi una fascia di valutazione</b> in base ai tuoi dati</p>
               </div>
             </div>
@@ -85,7 +85,7 @@ const Home = () => {
               <div>
                 <div className='bg-card rounded-full p-3 shadow-xl w-20 h-20 flex justify-center items-center'><UserRound className='h-10 w-10 text-foreground' /></div>
               </div>
-              <div className=' text-left text-lg font-medium text-zinc-800'>
+              <div className=' text-left text-lg font-medium text-zinc'>
                 <p><b>Consulenza</b> e valutazione precisa da parte di uno <b>dei nostri agenti</b></p>
               </div>
             </div>
