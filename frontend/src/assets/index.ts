e50e6d746f6ca9f7ab4b3f8f0fb353e3dac7edc4
Max <massimo.musso@edu-its.it>
2025-11-15 18:26:53 +0100

    Aggiunta sezione agenti alla home

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -9,9 +9,9 @@ import heroAltWebp from '@/assets/hero-alt-3.webp';
 import heroAltJpg from '@/assets/hero-alt-3.jpg';
 import about from '@/assets/about.png';
 import agent1 from '@/assets/agent1.jpg';
-import agent2 from '@/assets/agente2.jpg';
-import agent3 from '@/assets/agente3.jpg';
-import agent4 from '@/assets/agente4.jpg';
+import agent2 from '@/assets/agent2.jpg';
+import agent3 from '@/assets/agent3.jpg';
+import agent4 from '@/assets/agent4.jpg';
 
 export {
   heroAvif,
--- frontend/src/components/AgentCard.tsx
+++ frontend/src/components/AgentCard.tsx
@@ -1,17 +1,7 @@
 /**
- * Componets
- */
-import { Card } from '@/components/ui/card';
-
-/**
  * Icons
  */
 import { HomeIcon } from 'lucide-react';
-
-/**
- * Helper 
- */
-import { cn } from '@/lib/utils';
 
 interface AgentCardProps {
   agentName: string;
@@ -27,44 +17,27 @@ const AgentCard = ({
   soldProperties,
 }: AgentCardProps) => {
   return (
- <Card
-      className={cn(
-        "group relative h-[520px] overflow-hidden rounded-[40px] border-8 border-white shadow-2xl bg-white transition-all duration-500 aspect-9/16 mx-auto"
-      )}
-    >
-      
-      <div className="absolute overflow-hidden rounded-4xl">
+    <div className="bg-white w-full rounded-[50px] flex flex-col border-16 border-white shadow-2xl hover:scale-105 hover:shadow-primary transition-transform duration-1000">
+      <div>
         <img
           src={agentImage}
           alt={agentName}
-          className="w-full  object-cover transition-all duration-700 ease-in-out group-hover:h-full group-hover:scale-105"
+          className="h-full w-full rounded-[30px] aspect-square object-cover"
         />
       </div>
-
-      
-      <div className="absolute inset-0 flex flex-col justify-end p-6 bg-linear-to-t transition-all duration-700 group-hover:from-black/80 group-hover:via-black/30 group-hover:to-transparent">
-        <div
-          className={cn(
-            "transition-all duration-500 group-hover:text-white"
-          )}
-        >
-          <h3 className="text-2xl font-bold mb-1">{agentName}</h3>
-          <p className="text-gray-600 text-sm font-medium group-hover:text-gray-200">
-            {agentDescription}
-          </p>
-        </div>
-
-        <div className="flex gap-2 items-center mt-4">
-          <HomeIcon className="text-secondary h-5 w-5 group-hover:text-white" />
-          <p className="text-secondary font-semibold text-sm group-hover:text-gray-200">
+      <div className="px-4">
+        <h3 className="text-xl font-bold mt-5 text-black">{agentName}</h3>
+        <h4 className="text-light-grey font-medium text-md">
+          {agentDescription}
+        </h4>
+        <div className="flex gap-2 items-center justify-center mt-18">
+          <HomeIcon className="text-secondary h-5" />
+          <p className="text-secondary font-semibold text-center text-sm">
             {soldProperties}+ immobili venduti
           </p>
         </div>
       </div>
-    </Card>
-
-
-
+    </div>
   );
 };
 
