e19b424e4df2c062b1bdf73a2b2bb1b40d605307
davide-its <davide.martinelli@edu-its.it>
2025-11-17 11:46:54 +0100

    aggiunta di srcset e gestion immagine absolute

--- frontend/src/pages/Hero.tsx
+++ frontend/src/pages/Hero.tsx
@@ -1,4 +1,4 @@
-import { heroAltAvif, heroAltWebp, heroAltJpg, hero2Jpg } from '@/assets';
+import { hero2Avif, hero2Webp, hero2Jpg } from '@/assets';
 
 /**
  * Components
@@ -19,19 +19,33 @@ const Hero = ({ id }: HeroProps) => {
       <div
         className="relative h-[40vh] xl:h-[80vh] w-full flex justify-start items-start flex-col text-start mx-auto"
       >
-        {/* Mirrored background image */}
-        <div
-          className="absolute inset-0 pointer-events-none opacity-20 xl:opacity-100"
-          style={{
-            backgroundImage: `url(${hero2Jpg})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'top',
-            transform: 'scaleX(-1)',
-            transformOrigin: 'center',
-            zIndex: 0
-          }}
-        />
 
+        <figure className="absolute inset-0 pointer-events-none opacity-20 xl:opacity-100 w-full h-full bg-top"
+        >
+          <picture>
+            <source
+              srcSet={hero2Avif}
+              type="image/avif"
+            />
+            <source
+              srcSet={hero2Webp}
+              type="image/webp"
+            />
+            <img
+              src={hero2Jpg}
+              alt="Immagine di un appartamento"
+              loading="lazy"
+              style={{
+                backgroundPosition: 'top',
+                transform: 'scaleX(-1)',
+                transformOrigin: 'center',
+                zIndex: 0
+              }}
+              className='w-full h-full object-cover object-top!'
+
+            />
+          </picture>
+        </figure>
 
 
         <div className="container z-20 flex flex-col mx-auto justify-center xl:mt-48 p-10 text-start w-fit md:w-full h-full xl:h-fit">
