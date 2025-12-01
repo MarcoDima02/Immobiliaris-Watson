7a861475d1b10dd4bf2dd424bf145525ca82f31b
Max <massimo.musso@edu-its.it>
2025-12-01 20:20:00 +0100

    Migliorata logica dashboard agente

--- frontend/src/pages/AgentMyRequests.tsx
+++ frontend/src/pages/AgentMyRequests.tsx
@@ -1,20 +1,34 @@
+/**
+ * Node modules
+ */
+import { useLocation } from 'react-router';
+
+/**
+ * Components
+ */
 import { Button } from '@/components/ui/button';
 import AgentRequestDiv from '@/components/agentDashboard/AgentRequestDiv';
-import { useLocation } from 'react-router';
+
+/**
+ * Store
+ */
+import { useAuthStore } from '@/store/auth.store';
 
 const AgentMyRequests = () => {
   const location = useLocation();
-  const { requests } = location.state || {};
+  const { requests: stateRequests } = location.state || {};
 
-    const safeRequests = Array.isArray(requests) ? requests : [];
+
+  const allRequests = useAuthStore((s) => s.agentDashboard) || [];
+  const requests = stateRequests ?? allRequests;
 
   if (requests === null) return <p>Caricamento dashboard...</p>;
-  if (safeRequests.length === 0) return <p>Nessun contratto trovato</p>;
+  if (requests.length === 0) return <p>Nessun contratto trovato</p>;
 
   return (
     <>
       <Button onClick={() => history.back()}>Indietro</Button>
-      <h2 className="text-2xl font-bold mt-5">Richieste prese in carico:</h2>
+      <h2 className="text-2xl font-bold mt-5">Richieste:</h2>
       <div className="flex gap-4 flex-wrap py-4">
         {requests.map((request: any) => (
           <>
